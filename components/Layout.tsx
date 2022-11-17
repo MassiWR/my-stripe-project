10cfc8dbb620842a4306e2659aac02a8b512430f
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-11-17 14:13:11 +0100

    add config file

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,32 +1,37 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
 
-export default Layout
+const Layout = ({ children, title = "This is the default title" }: Props) => (
+  <Elements stripe={stripePromise}>
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
+          <Link href="/users">Users List</Link> |{" "}
+          <a href="/api/users">Users API</a>
+        </nav>
+      </header>
+      {children}
+    </div>
+  </Elements>
+);
+
+export default Layout;
